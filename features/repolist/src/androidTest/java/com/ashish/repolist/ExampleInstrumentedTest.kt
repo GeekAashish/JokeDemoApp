213211faa8cab009db824c76097308f9110a042e
ashishk@techaheadcorp.com <9015286806@As>
2023-10-12 03:21:53 +0530

    bug fixed

--- features/repolist/src/androidTest/java/com/ashish/repolist/ExampleInstrumentedTest.kt
+++ features/repolist/src/androidTest/java/com/ashish/repolist/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.iamkamrul.repolist.test", appContext.packageName)
+        assertEquals("com.ashish.repolist.test", appContext.packageName)
     }
 }
